00f793b399570f71e54478bbe29b35422bd440cb
DotCipher <dotcipher@mail.dev>
2024-03-21 16:19:22 +0000

    boilerplate spring framework

---
+++ src/test/java/com/dotcipher/securingspring/SecuringSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotcipher.securingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
